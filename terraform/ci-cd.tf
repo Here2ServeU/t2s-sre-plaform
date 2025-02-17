ebcaae325870526327e06f081d2d269678d275bd
Learn DevOps & Cloud <revnaweji@gmail.com>
2025-02-17 10:02:50 -0600

    Update ci-cd.tf

--- terraform/ci-cd.tf
+++ terraform/ci-cd.tf
@@ -1,6 +1,6 @@
 # CI/CD Integration with Terraform Workspaces
 resource "null_resource" "terraform_apply" {
   provisioner "local-exec" {
-    command = "terraform workspace select ${local.environment} || terraform workspace new ${local.environment}"
+    command = "terraform workspace select ${var.environment} || terraform workspace new ${var.environment}"
   }
 }
