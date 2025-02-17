21554328408d609f9eae115291e9a96412b4ddf3
Learn DevOps & Cloud <revnaweji@gmail.com>
2025-02-17 13:44:21 -0600

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+# src/App.js - React App Source Code
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to T2S Frontend</h1>
+    </div>
+  );
+}
+
+export default App;
